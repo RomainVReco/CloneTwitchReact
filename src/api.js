b08befc326129801d306c14cf51f8fd32978c591
RomainVReco <145270898+RomainVReco@users.noreply.github.com>
2023-11-17 15:18:23 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 let api = axios.create({
     headers:{
-        'Client-ID' : 'yp312bzoesdqgp3f1mp4ut4mt1hkcc',
-        'Authorization' : 'Bearer rfwhcna4m8kmpg0qw7u7ilatw2p4u1'
+        'Client-ID' : '',
+        'Authorization' : 'Bearer ''
     }
 })
 
