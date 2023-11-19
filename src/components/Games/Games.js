afdef56e6cf973652f711a0393ea7099f0ae69a4
Romain VORLET <romain.vorlet@gmail.com>
2023-11-19 20:25:44 +0100

    Ajout api sidebar

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -9,8 +9,17 @@ function Games() {
     useEffect(() => {
             const fetchData = async () => {
               const result = await api.get('https://api.twitch.tv/helix/games/top')
-              console.log(result)
-           
+              console.log("API Top Games : "+ result)
+
+              let dataArray = result.data.data
+              let finalArray = dataArray.map(game => {
+                let newUrl = game.box_art_url
+                .replace("{width}", "250")
+                .replace("{height}", "300")
+                game.box_art_url = newUrl
+                return game
+              })
+                setGames(finalArray)
               }
             fetchData();
     }, [])
@@ -18,7 +27,17 @@ function Games() {
   return (
       <div>
           <h1 className="titreGames">Jeux les plus populaires</h1>
-          <div className="flexaccueil">
+          <div className="flexAccueil">
+            {games.map((game, index) => (
+              <div key={index} className="carteGames">
+                <img src={game.box_art_url} alt="jeu profile pic" className="imgCarte" />
+                <div className="cardBodyGames">
+                  <h5 className="titreCartesGames">{game.name}</h5>
+                  <div className="btnCarte">Regarder {game.name}</div>
+                </div>
+
+              </div>
+            ))}
 
 
           </div>
