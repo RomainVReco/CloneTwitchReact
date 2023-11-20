def02742a5a6dc1a72cc1cd8ca503281daf59e2a
Romain VORLET <romain.vorlet@gmail.com>
2023-11-20 11:57:55 +0100

    ajout top Streams et liens header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from './IconeTwitch.svg'
 import search from './Search.svg'
 import menuIco from './MenuIco.svg'
+import {Link} from 'react-router-dom'
 
 function Header() {
 
@@ -12,13 +13,19 @@ function Header() {
             <nav className="headerTop"> 
             <ul className="listeMenu">
                 <li className="liensNav">
-                    <img src={logo} alt="logo twitch" className="logo" />
+                    <Link className='lien' to="/">
+                        <img src={logo} alt="logo twitch" className="logo" />
+                    </Link>
                 </li>
                 <li className="liensNav">
+                    <Link className='lien' to="/">
                     Top Games
+                    </Link>
                 </li>
                 <li className="liensNav">
-                    Top Streams
+                    <Link className='lien' to="/top-streams">
+                        Top Streams
+                    </Link>
                 </li>
                 <li className="liensNav">
                     <form action="" className="formSubmit">
---
+++ src/components/TopStreams/TopStreams.js
@@ -0,0 +1,73 @@
+import React, {useEffect, useState} from 'react';
+import api from '../../api';
+
+
+function TopStreams(){
+
+    const[channels, setChannels] = useState([])
+
+    useEffect(() => {
+
+        const fetchDataStream = async () => {
+            const result = await api.get('https://api.twitch.tv/helix/streams')
+            console.log("API Top Streams : "+ result)
+            let dataArray = result.data.data
+            let topStreams = dataArray.map(stream => {
+                return stream.user_id
+            })
+            let tempUrl = ''
+            for (let i=0; i<topStreams.length;i++) {
+                tempUrl += `id=${topStreams[i]}&`
+            }
+            var userUrl = tempUrl.slice(0,tempUrl.length-1)
+
+
+            const resultUsers = await api.get('https://api.twitch.tv/helix/users?'+userUrl)
+            var dataArrayUsers = resultUsers.data.data
+            let i = 0
+            let finalArray = dataArray.map(stream => {
+                stream.truepic = dataArrayUsers[i].profile_image_url
+                let newUrl = stream.thumbnail_url
+                .replace('{width}', "320")
+                .replace('{height}', "180")
+                stream.thumbnail_url = newUrl
+                i += 1
+                return stream
+            })
+
+            setChannels(finalArray)
+        }
+
+        fetchDataStream()
+    }, [])
+
+//http://localhost:3000/#access_token=u61osxv9f2i52hn8osgkovow33p4cj&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&token_type=bearer
+
+    return (
+        <div>
+            <h1 className='titreGames'>Stream les plus populaires</h1>
+            <div className="flexAccueil">
+                {channels.map((channel, index) => (
+                    <div key={index} className="carteStream">
+                        <img src={channel.thumbnail_url} className="imgCarte"  alt="jeu" />
+                        <div className="cardBodyStream">
+                            <h5 className="titreCartesStream">{channel.user_name}</h5>
+                            <p className="textStream">Jeu : {channel.gameName}</p>
+                            <p className="txtStreamViewver">Viewers : {channel.viewer_count}</p>
+                            <div className="btnCarte">Regarder {channel.user_name}</div>
+                        </div>
+
+
+                    </div>
+
+
+
+                ))}
+            </div>
+        </div>
+        )
+
+}
+
+
+export default TopStreams
